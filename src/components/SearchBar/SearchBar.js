bcc1e32f383d652ebf36fabd28e9ad14e77e7851
Vladimir <git_109@list.ru>
2021-08-12 20:05:40 +0300

    create a state to keep term of the search bar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,10 @@ import React from 'react'
 import './SearchBar.css'
 
 export class SearchBar extends React.Component {
+  state = {
+    term: ''
+  }
+
   search = () => this.props.onSearch(this.state.term)
         <button className="SearchButton" onClick={this.search}>
           SEARCH
