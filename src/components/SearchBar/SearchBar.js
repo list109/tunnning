132e2e73400b0917d3585181b5e6c32b90751ee9
Vladimir <git_109@list.ru>
2021-08-12 20:04:28 +0300

    turn the search bar function into a class

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './SearchBar.css'
 
+export class SearchBar extends React.Component {
   search = () => this.props.onSearch(this.state.term)
         <button className="SearchButton" onClick={this.search}>
           SEARCH
