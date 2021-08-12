a240056e680ffb90563dab4963a052b6a6bc177a
Vladimir <git_109@list.ru>
2021-08-12 20:07:54 +0300

    a typical method for the search bar to precess an input value change

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,6 +7,17 @@ export class SearchBar extends React.Component {
   }
 
   search = () => this.props.onSearch(this.state.term)
+
+  handleTermChange = ({ target }) => this.setState({ term: target.value })
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input
+          value={this.state.term}
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleTermChange}
+        />
         <button className="SearchButton" onClick={this.search}>
           SEARCH
         </button>
