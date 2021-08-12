c455ac33cf2db6c4cb53737e00584d0e2d09fe28
Vladimir <git_109@list.ru>
2021-08-12 20:03:33 +0300

    create methods to pass the term value of the search bar's state to the main search method when user has started search with the button

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './SearchBar.css'
 
-export function SearchBar() {
-  return (
-    <div className="SearchBar">
-      <input placeholder="Enter A Song, Album, or Artist" />
-      <button className="SearchButton">SEARCH</button>
-    </div>
-  )
+  search = () => this.props.onSearch(this.state.term)
+        <button className="SearchButton" onClick={this.search}>
+          SEARCH
+        </button>
+      </div>
+    )
+  }
 }
