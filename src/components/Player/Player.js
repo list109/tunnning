3332ff28ac06afebe9f6295dd94911ba7085cdd0
Vladimir <git_109@list.ru>
2021-10-17 19:49:12 +0300

    the player's event handlers to preprocess the volume/playback state as it is changed

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,6 +3,26 @@ import './Player.css'
 import * as icons from './icons/icons'
 
 export class Player extends React.Component {
+  getStateHandler = callback => {
+    return e => {
+      const fraction = this.getStateFraction(e)
+      const percentages = (fraction * 100).toFixed(1)
+      const { firstElementChild: child } = e.currentTarget
+      child.style.width = `${
+        (percentages > 95 && 100) || (percentages < 5 && 0.01) || percentages
+      }%`
+
+      callback()
+    }
+  }
+
+  getStateFraction = ({ currentTarget: elem, pageX }) => {
+    const { left, right } = elem.getBoundingClientRect()
+    const width = right - left
+    const relPos = pageX - left
+    return relPos / width
+  }
+
   render() {
     const {
       paused,
@@ -28,17 +48,17 @@ export class Player extends React.Component {
           <button className="player-forward" type="button" onClick={onForwardClick}>
             {icons.forward}
           </button>
-          <div className="player-playback-total" onClick={onPlaybackClick}>
+          <div className="player-playback-total" onClick={this.getStateHandler(onPlaybackClick)}>
             <span className="player-playback-passed" style={{ width: '25%' }}></span>
           </div>
           <p className="player-time">
-            <time className="player-passed">00:14</time>
-            <time className="player-duration">00:31</time>
+            <time className="player-time-passed">00:14</time>
+            <time className="player-time-duration">00:31</time>
           </p>
           <button className="player-mute" type="button" onClick={onMuteClick}>
             {muted ? icons.mute : icons.volume}
           </button>
-          <div className="player-volume-total" onClick={onVolumeClick}>
+          <div className="player-volume-total" onClick={this.getStateHandler(onVolumeClick)}>
             <span className="player-volume-level" style={{ width: '25%' }}></span>
           </div>
         </div>
