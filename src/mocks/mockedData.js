1b1cd9f24ab62513a4e656185939733f3c770787
Vladimir <git_109@list.ru>
2022-12-12 15:57:59 +0300

    update mock data

--- src/mocks/mockedData.js
+++ src/mocks/mockedData.js
@@ -11,7 +11,7 @@ const data = {
       artists: [{ name: "Cassette" }],
       album: { name: "Album 1" },
       preview_url:
-        "https://40s.musify.club/get/69/25508826/64056776/1/1/0/cassette-tell-me-why.mp3",
+        "https://musify.club/track/play/17840955/cassette-tell-me-why.mp3",
     },
     {
       id: "2",
@@ -19,7 +19,7 @@ const data = {
       artists: [{ name: "BLACKPINK" }],
       album: { name: "Album 2" },
       preview_url:
-        "https://37ts.musify.club/get/70/26324288/65712290/1/1/0/blackpink-shut-down.mp3",
+        "https://39s.musify.club/get/68/26324288/65750559/1/1/0/blackpink-shut-down.mp3",
     },
     {
       id: "3",
@@ -31,91 +31,91 @@ const data = {
     },
     {
       id: "4",
-      name: "Полина Гагарина",
-      artists: [{ name: "Вода" }],
+      name: "Вода",
+      artists: [{ name: "Полина Гагарина" }],
       album: { name: "Album 4" },
       preview_url:
         "https://musify.club/track/play/18381548/%d0%bf%d0%be%d0%bb%d0%b8%d0%bd%d0%b0-%d0%b3%d0%b0%d0%b3%d0%b0%d1%80%d0%b8%d0%bd%d0%b0-%d0%b2%d0%be%d0%b4%d0%b0.mp3",
     },
     {
       id: "5",
-      name: "David Guetta ft.Robin Schulz",
+      name: "On Repeat",
       artists: [{ name: "David Guetta ft.Robin Schulz" }],
       album: { name: "Album 5" },
       preview_url:
-        "https://musify.club/track/play/18273071/robin-schulz-david-guetta-robin-schulz-and-david-guetta-on-repeat.mp3",
+        "https://41s.musify.club/get/68/26517248/66179552/1/1/0/robin-schulz-david-guetta-on-repeat.mp3",
     },
     {
       id: "6",
-      name: "Tell Me Why",
-      artists: [{ name: "Cassette" }],
+      name: "All By Myself",
+      artists: [{ name: "Ellie Goulding ft. Alok ft. Sigala" }],
       album: { name: "Album 1" },
       preview_url:
-        "https://40s.musify.club/get/69/25508826/64056776/1/1/0/cassette-tell-me-why.mp3",
+        "https://41s.musify.club/get/69/26437186/66079558/1/1/0/alok-sigala-ellie-goulding-all-by-myself.mp3",
     },
     {
       id: "7",
-      name: "Shut Down",
-      artists: [{ name: "BLACKPINK" }],
+      name: "Summer Really Hurt Us",
+      artists: [{ name: "Alma" }],
       album: { name: "Album 2" },
       preview_url:
-        "https://37ts.musify.club/get/70/26324288/65712290/1/1/0/blackpink-shut-down.mp3",
+        "https://38s-a.musify.club/get/70/26380790/65847830/1/1/0/alma-summer-really-hurt-us.mp3",
     },
     {
       id: "8",
-      name: "Движ",
-      artists: [{ name: "Filatov & Karas" }],
+      name: "Heartbreaker",
+      artists: [{ name: "KDDK " }],
       album: { name: "Album 3" },
       preview_url:
-        "https://40s.musify.club/get/69/26086361/65198364/1/1/0/filatov-and-karas-%d0%b4%d0%b2%d0%b8%d0%b6.mp3",
+        "https://37s-a.musify.club/get/71/26273851/65621012/1/1/0/kddk-heartbreaker.mp3",
     },
     {
       id: "9",
-      name: "Полина Гагарина",
-      artists: [{ name: "Вода" }],
+      name: "Million Dollar Baby",
+      artists: [{ name: "Ava Max" }],
       album: { name: "Album 4" },
       preview_url:
-        "https://musify.club/track/play/18381548/%d0%bf%d0%be%d0%bb%d0%b8%d0%bd%d0%b0-%d0%b3%d0%b0%d0%b3%d0%b0%d1%80%d0%b8%d0%bd%d0%b0-%d0%b2%d0%be%d0%b4%d0%b0.mp3",
+        "https://41s.musify.club/get/69/26264615/65590943/1/1/0/ava-max-million-dollar-baby.mp3",
     },
     {
       id: "10",
-      name: "David Guetta ft.Robin Schulz",
-      artists: [{ name: "David Guetta ft.Robin Schulz" }],
+      name: "Если Сгорим",
+      artists: [{ name: "NЮ" }],
       album: { name: "Album 5" },
       preview_url:
-        "https://musify.club/track/play/18273071/robin-schulz-david-guetta-robin-schulz-and-david-guetta-on-repeat.mp3",
+        "https://41s-a.musify.club/get/71/26718767/66559720/1/1/0/n%d1%8e-%d0%b5%d1%81%d0%bb%d0%b8-%d1%81%d0%b3%d0%be%d1%80%d0%b8%d0%bc.mp3",
     },
     {
       id: "11",
-      name: "Tell Me Why",
-      artists: [{ name: "Cassette" }],
+      name: "Tanimadingmi",
+      artists: [{ name: "Божалар ft.VIA Marokand" }],
       album: { name: "Album 1" },
       preview_url:
-        "https://40s.musify.club/get/69/25508826/64056776/1/1/0/cassette-tell-me-why.mp3",
+        "https://40s-a.musify.club/get/70/24365865/61673860/1/1/0/%d0%b1%d0%be%d0%b6%d0%b0%d0%bb%d0%b0%d1%80-via-marokand-tanimadingmi.mp3",
     },
     {
       id: "12",
-      name: "Shut Down",
-      artists: [{ name: "BLACKPINK" }],
+      name: "Повело",
+      artists: [{ name: "Anna Asti" }],
       album: { name: "Album 2" },
       preview_url:
-        "https://37ts.musify.club/get/70/26324288/65712290/1/1/0/blackpink-shut-down.mp3",
+        "https://41s.musify.club/get/68/26023927/65080536/1/1/0/anna-asti-%d0%bf%d0%be%d0%b2%d0%b5%d0%bb%d0%be.mp3",
     },
     {
       id: "13",
-      name: "Движ",
-      artists: [{ name: "Filatov & Karas" }],
+      name: "2 Die 4",
+      artists: [{ name: "Tove Lo" }],
       album: { name: "Album 3" },
       preview_url:
-        "https://40s.musify.club/get/69/26086361/65198364/1/1/0/filatov-and-karas-%d0%b4%d0%b2%d0%b8%d0%b6.mp3",
+        "https://40s.musify.club/get/69/26217809/65506329/1/1/0/tove-lo-2-die-4.mp3",
     },
     {
       id: "14",
-      name: "Полина Гагарина",
-      artists: [{ name: "Вода" }],
+      name: "So Lonely",
+      artists: [{ name: "Kush Kush" }],
       album: { name: "Album 4" },
       preview_url:
-        "https://musify.club/track/play/18381548/%d0%bf%d0%be%d0%bb%d0%b8%d0%bd%d0%b0-%d0%b3%d0%b0%d0%b3%d0%b0%d1%80%d0%b8%d0%bd%d0%b0-%d0%b2%d0%be%d0%b4%d0%b0.mp3",
+        "https://41s.musify.club/get/68/26252721/65574271/1/1/0/kush-kush-so-lonely.mp3",
     },
   ],
 };
